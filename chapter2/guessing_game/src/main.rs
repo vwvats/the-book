c6960f59828414686d413f6bb20e0472f829f566
Vivek Vats <vwvats@gmail.com>
2022-06-01 02:15:26 +0530

    finished the guessing game

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,23 +1,31 @@
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use colored::*;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
     
-    println!("Enter your guess:");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-    let guess: u32 = guess.trim().parse().expect("Please enter a number."); 
-    
-    println!("You guessed {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Greater => println!("Too Big"),
-        Ordering::Equal => println!("You Win!"),
+    loop {
+        println!("Enter your guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You guessed {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too Small".red()),
+            Ordering::Greater => println!("{}", "Too Big".red()),
+            Ordering::Equal => {
+                println!("{}", "You Win!".green());
+                break;
+            },
+        }
     }
 }
