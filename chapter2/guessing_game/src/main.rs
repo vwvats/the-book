206c97a36d9d1697437990d822552538aa54b46c
Vivek Vats <vwvats@gmail.com>
2022-06-01 02:03:51 +0530

    finished the logic for guessing game

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,14 +1,23 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret is {}", secret_number);
+    
     println!("Enter your guess:");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line");
+        .expect("Failed to read line.");
+    let guess: u32 = guess.trim().parse().expect("Please enter a number."); 
+    
     println!("You guessed {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You Win!"),
+    }
 }
